d2b95dae22e39c78b3ee4df323bfbefd2067a453
DanBaulk <danielbaulk@gmail.com>
2025-02-04 10:22:11 +0000

    initial blog

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
+import linkedinLogo from './assets/linkedin.png';
+import githubLogo from './assets/github.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="banner">
+        <div className="banner-title">B(aulk)log</div>
+        <div className="banner-links">
+          <a href="https://www.linkedin.com/in/danielbaulk/" target="_blank" rel="noopener noreferrer">
+            <img src={linkedinLogo} alt="LinkedIn" className="banner-logo" />
+          </a>
+          <a href="https://github.com/danbaulk" target="_blank" rel="noopener noreferrer">
+            <img src={githubLogo} alt="GitHub" className="banner-logo" />
+          </a>
+        </div>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="intro">
+          <h2>Welcome to B(aulk)log</h2>
+          <p>This is a blog to share the various projects I'm working on. Stay tuned for updates!</p>
+        </section>
       </header>
     </div>
   );
