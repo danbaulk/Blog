f46e93c9d054b965a7cad2d82e19dcd0efd7bd82
DanBaulk <danielbaulk@gmail.com>
2025-02-06 12:55:32 +0000

    implement carousel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import logo from './assets/logo.svg';
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+
 import linkedinLogo from './assets/linkedin.png';
 import githubLogo from './assets/github.png';
+import fplLogo from './assets/fpl-logo.png';
+import comingSoon from './assets/coming-soon.png';
+
 import './App.css';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
 
 function App() {
   return (
@@ -10,20 +35,42 @@ function App() {
         <div className="banner-title">B(aulk)log</div>
         <div className="banner-links">
           <a href="https://www.linkedin.com/in/danielbaulk/" target="_blank" rel="noopener noreferrer">
-            <img src={linkedinLogo} alt="LinkedIn" className="banner-logo" />
+            <img src={linkedinLogo} alt="LinkedIn" className="banner-logo"/>
           </a>
           <a href="https://github.com/danbaulk" target="_blank" rel="noopener noreferrer">
-            <img src={githubLogo} alt="GitHub" className="banner-logo" />
+            <img src={githubLogo} alt="GitHub" className="banner-logo"/>
           </a>
         </div>
       </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <section className="intro">
-          <h2>Welcome to B(aulk)log</h2>
+          <h2>Welcome to my B(aulk)log</h2>
           <p>This is a blog to share the various projects I'm working on. Stay tuned for updates!</p>
         </section>
       </header>
+      <section className="carousel-section">
+        <h2>My Projects</h2>
+        <Carousel responsive={responsive} autoPlay={true} infinite={true}>
+          <div>
+            <img src={fplLogo} alt="FPL Assitant Manager" className="fpl-logo"/>
+            <h3>FPL Assitant Manager</h3>
+            <p>This project extended the work I did for my
+              Final Year Project in University, I've trained multiple ANN models on a large historic FPL dataset
+              and created a backend golang API which interacts with the offical FPL API to get the latest data
+              and predict the highest scoring team for the upcoming gameweek.</p>
+          </div>
+          <div>
+            <img src={comingSoon} alt="Blog Post 2" className="comingsoon-logo"/>
+            <h3>Blog Post 2</h3>
+            <p>Coming Soon</p>
+          </div>
+          <div>
+            <img src={comingSoon} alt="Blog Post 3" className="comingsoon-logo"/>
+            <h3>Blog Post 3</h3>
+            <p>Coming Soon</p>
+          </div>
+        </Carousel>
+      </section>
     </div>
   );
 }
